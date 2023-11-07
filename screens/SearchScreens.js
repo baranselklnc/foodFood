8295f985786c8249119eb02b92944fde8ccb2f25
baranselklnc <baranselklnc@gmail.com>
2023-11-07 12:03:32 +0300

    initialize project

---
+++ screens/SearchScreens.js
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export default function SearchScreens() {
+  return (
+    <View>
+      <Text>SearchScreens</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
